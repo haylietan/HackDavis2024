085045bd63d25d2ba87755d3f314e1f92e8eb010
Haylie Tan <141064325+haylietan@users.noreply.github.com>
2024-04-27 15:53:46 -0700

    Created pages

--- hackdavis2024/src/App.js
+++ hackdavis2024/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path="/aboutme" element={<AboutMe/>}/>
+          <Route path='/projects' element={<Projects/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+          <Route path='/*' element={<Error/>}/>
+        </Routes>
+      </Router>
+      <Footer/>
     </div>
   );
 }
+
 
 export default App;
